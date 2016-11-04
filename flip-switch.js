c0e5c6e8134576474a4b024fc895e2ee8ef3872a
Mathieu <mathieu0709@gmail.com>
2016-11-04 12:00:59 +0100

    wip

--- flip-switch.js
+++ flip-switch.js
@@ -11,13 +11,23 @@ customElements.define("flip-switch", class extends HTMLElement {
         const tmpl = doc.querySelector("#fs-tmpl");
         this._root = this.attachShadow({mode: 'open'});
         this._root.appendChild(tmpl.content.cloneNode(true));
+
+        this._container = this._root.querySelector('.container');
     }
 
     connectedCallback() {
-            
+        this._addEventListeners();
     }
 
     disconnectedCallback() {
 
+    }
+
+    flip() {
+        this._container.classList.toggle('.flipped');
+    }
+
+    _addEventListeners () {
+        document.addEventListener('click', () => this.flip());
     }
 })
