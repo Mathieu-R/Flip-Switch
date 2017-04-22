67ab118f6ef01c7e289c18dd1212b38c38f1a654
Mathieu-R <mathieu0709@gmail.com>
2017-04-22 20:23:56 +0200

    init

--- flip-switch.js
+++ flip-switch.js
@@ -1,6 +1,4 @@
-"use strict";
-
-customElements.define("flip-switch", class extends HTMLElement {
+class FlipSwitch extends HTMLElement {
     static get observedAttributes () {
         return [];
     }
@@ -8,27 +6,9 @@ customElements.define("flip-switch", class extends HTMLElement {
     constructor () {
         super();
         const doc = document.currentScript.ownerDocument;
-        const tmpl = doc.querySelector("#fs-tmpl");
+        const template = doc.querySelector("#fs-tmpl");
         this._root = this.attachShadow({mode: 'open'});
-        this._root.appendChild(tmpl.content.cloneNode(true));
-        
-        this._container = this._root.querySelector('.container');
-        this._front = this._root.querySelector('.front');
-        this._back = this._root.querySelector('.back');
-        this._ripple = this._root.querySelector('.ripple');
-        this._shadow2px = this._root.querySelector('.shadow2px');
-
-        this._onResize = this._onResize.bind(this);
-        this._onResize();
-    }
-
-    set value(_value) {
-        this._value = _value;
-        this._front.querySelector('button').textContent = _value;
-    }
-
-    get value() {
-        return this._value;
+        this._root.appendChild(template.content.cloneNode(true));
     }
 
     connectedCallback() {
@@ -40,34 +20,16 @@ customElements.define("flip-switch", class extends HTMLElement {
     }
 
     flip() {
-        this._container.classList.toggle('flipped');
-        this._ripple.classList.toggle('expanded');
-        this._shadow2px.classList.toggle('flipped')
+        
     }
 
     _addEventListeners () {
-        this._front.addEventListener('click', () => this.flip());
-        this._back.addEventListener('click', e => {
-
-            // If we clic on the back
-            if (e.target === e.currentTarget) {
-                return;
-            }
-
-            this.value = e.target.textContent;
-            this.flip();
-        })
-
-        window.addEventListener('resize', this._onResize);
+        
     }
 
     _onResize () {
-        const midX = window.innerHeight * 0.5;
-        const midY = window.innerHeight * 0.5;
-
-        const radius = Math.sqrt(midX * midX + midY * midY);
-
-        this._ripple.style.width = `${radius * 2}px`;
-        this._ripple.style.height = `${radius * 2}px`;
+        
     }
-})
+}
+
+customElements.define("flip-switch", FlipSwitch);
